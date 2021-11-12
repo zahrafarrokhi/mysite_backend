68ba9c389f5e6262afde62a0b739582b0e9c7766
yasi <zahrafarrokhi2017@gmail.com>
2021-11-12 20:06:57 +0330

    Add files

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,3 +23,8 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('contacts/', include('contact.urls')),
 ]
+
+# add root static files
+# urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# add media static files
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
